aadd02521540cf99fa5cd510eb358915f1983586
YAMASHITA Yuta <proyuuta0618188188@gmail.com>
2022-01-07 14:31:58 +0900

    OK

--- yubrary/yu_requests.py
+++ yubrary/yu_requests.py
@@ -1,20 +1,20 @@
 import requests
 
 
-def yu_requests(url, cookie = None , bool = False):
+def yu_requests(url, cookie = None):
     '''
     入力
-    第一引数 URL 第二引数 cookie 第三引数 bool(クッキーを返すか？デフォは返さない)
+    第一引数 URL 第二引数 cookie 
     出力
     URLを投げるとBeautifulSoupに入れるテキストを第一引数
     別のページに飛ばされていないかをbool型(True=飛ばされていない)第二引数
-    cookieを第三引数(入力で設定しなければNoneを返す)
+    cookieを第三引数
     '''
     header = user_agent()
     res  = requests.get(url,cookies=cookie,headers=header)
     text = res.text
     urlcheck = url_check(res ,url)
-    new_cookie = get_cookie(res,bool)
+    new_cookie = res.cookies.get_dict()
     if res.status_code != 200:
         raise YubraryError("ステータスコードが200じゃないよ")
     return text, urlcheck, new_cookie
@@ -30,13 +30,6 @@ def url_check(res, url):
     if res.url != url:
         return False
     return True
-
-
-def get_cookie(res, bool):
-    if not bool:
-        return None
-    cookie = res.cookies.get_dict()
-    return cookie
 
 
 class YubraryError(Exception):
