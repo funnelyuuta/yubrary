bfa20bc287bfb2dafdd3598505f86140decc1eb2
YAMASHITA Yuta <proyuuta0618188188@gmail.com>
2022-01-07 14:18:29 +0900

    あ

--- yubrary/yu_requests.py
+++ yubrary/yu_requests.py
@@ -1,4 +1,6 @@
 import requests
+
+
 def yu_requests(url, cookie = None , bool = False):
     '''
     入力
@@ -6,32 +8,29 @@ def yu_requests(url, cookie = None , bool = False):
     出力
     URLを投げるとBeautifulSoupに入れるテキストを第一引数
     別のページに飛ばされていないかをbool型(True=飛ばされていない)第二引数
-    ステータスコードの状態をbool型(True=ステータスコード200)第三引数
-    cookieを第四引数(入力で設定しなければNoneを返す)
+    cookieを第三引数(入力で設定しなければNoneを返す)
     '''
     header = user_agent()
     res  = requests.get(url,cookies=cookie,headers=header)
     text = res.text
-    status = statuscode(res)
     urlcheck = url_check(res ,url)
     new_cookie = get_cookie(res,bool)
+    if res.status_code != 200:
+        raise YubraryError("ステータスコードが200じゃないよ")
+    return text, urlcheck, new_cookie
 
-    return text, status, urlcheck, new_cookie  
 
 def user_agent():
     ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
     header = {'User-Agent': ua}
     return header
 
-def statuscode(res):
-    if res.status_code != 200:
-        return False
-    return True
 
 def url_check(res, url):
     if res.url != url:
         return False
     return True
+
 
 def get_cookie(res, bool):
     if not bool:
@@ -40,3 +39,5 @@ def get_cookie(res, bool):
     return cookie
 
 
+class YubraryError(Exception):
+    pass
